d262b022df3bcfb8286c5f3116467bd86c0d7e6f
MRdima98 <dimafrunza69@gmail.com>
2024-10-04 07:45:19 +0200

    Stupid

--- main.go
+++ main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
-	"log"
+	"os"
 
-	_ "github.com/lib/pq"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	connStr := "user= dbname=food sslmode=disable password="
-	db, err := sql.Open("postgres", connStr)
+	urlExample := "postgres://dima:dima@localhost:5432/food"
+	conn, err := pgx.Connect(context.Background(), urlExample)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
 	}
-	row, err := db.Query("select name from recipes limit 1")
+	defer conn.Close(context.Background())
+
+	var name string
+	err = conn.QueryRow(context.Background(), "select name from recipes limit 1;").Scan(&name)
 	if err != nil {
-		log.Fatal("Execute: ", err)
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
 	}
-	row.Scan()
-	fmt.Println(row.Next())
-	fmt.Println(row)
+
+	fmt.Println(name)
 }
