e9066049ab2ede4506697bab99602233f04cbe98
MRdima98 <dimafrunza69@gmail.com>
2024-10-24 06:59:47 +0200

    Home page working as intended

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,21 @@
 window.addEventListener("load", function () {
   let menuButton = document.getElementById("menu-button");
   let menu = document.getElementById("menu");
+  let fakeBody = document.getElementById("body");
 
-  menuButton.addEventListener("click", () => {
-    menu.style.visibility = "visible";
+  fakeBody.addEventListener("click", () => {
+    menu.classList.remove("transform-none");
+    fakeBody.classList.remove("blur-lg");
   });
 
-  addEventListener("visibilitychange", () => {
-    console.log("spooky");
+  menuButton.addEventListener("click", () => {
+    let timer = null;
+    if (timer !== null) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function () {
+      menu.classList.add("transform-none");
+      fakeBody.classList.add("blur-lg");
+    }, 25);
   });
 });
