1f388d96507f836af219ab43a68ffcd075307b08
MRdima98 <dimafrunza69@gmail.com>
2024-10-30 07:16:46 +0100

    Also part of submit

--- static/js/pictures.js
+++ static/js/pictures.js
@@ -1,3 +1,20 @@
 window.addEventListener("load", function () {
-  alert("we are here!");
+  let pictureLabel = document.getElementById("picture-label");
+  let pictureInput = document.getElementById("picture-input");
+  let uploadButton = document.getElementById("upload-button");
+  let undo = document.getElementById("undo");
+
+  pictureLabel.addEventListener("click", () => {
+    pictureInput.click();
+  });
+
+  pictureInput.addEventListener("change", () => {
+    pictureLabel.style.display = "none";
+    uploadButton.style.display = "flex";
+  });
+
+  undo.addEventListener("click", () => {
+    pictureLabel.style.display = "inline";
+    uploadButton.style.display = "none";
+  });
 });
